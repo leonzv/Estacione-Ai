fb1023fa926fbf7020c3285587396e96848979d0
Leonardo Vinicius Sanchez Dias <60626709+leonzv@users.noreply.github.com>
2021-05-26 16:13:09 -0300

    Add files via upload

--- src/views/gps.js
+++ src/views/gps.js
@@ -1,10 +1,14 @@
-import React, { Component } from "react";
-import "react-native-gesture-handler";
-import { View, Text, TouchableOpacity } from "react-native";
-import Style from "../style/style.js";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import Geolocation from "react-native-geolocation-service";
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import ModalGps from "../components/modalGps";
+import Style from "../style/style.js";
+import "react-native-gesture-handler";
+import React, { Component } from "react";
+import {FONTS,COLORS,SIZES} from '../constants';
+const Gps = ({route, navigation}) => {
+
+const GOOGLE_API_KEY = 'AIzaSyDaJUFUIRPgP0kPOCF350ZOqPoYmPxrZ-w';
 var mapStyle = [
   {
     elementType: "geometry",
@@ -211,48 +215,127 @@ var mapStyle = [
     ],
   },
 ];
+  const mapView = React.useRef();
 
-const hasLocationPermission = true;
-async function requestPermissions() {
-  if (Platform.OS === "ios") {
-    Geolocation.requestAuthorization();
-    Geolocation.setRNConfiguration({
-      skipPermissionRequests: false,
-      authorizationLevel: "whenInUse",
-    });
-  }
+  const [region, setRegion] = React.useState(mapRegion);
+  const [duration, setDuration] = React.useState(0);
+  const [isReady, setIsReady] = React.useState(false);
+  const [angle, setAngle] = React.useState(0);
 
-  if (Platform.OS === "android") {
-    await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+  const currentLocation = {
+    streetName: 'Star Park',
+    gps: {
+      latitude: -23.5489,
+      longitude: -46.6382,
+    },
+  };
+  const vaga = {
+    gps: {
+      latitude: -23.5442,
+      longitude: -46.6312,
+    },
+  };
+    const fromLoc = currentLocation.gps;
+    const toLoc = vaga.gps;
+    const street = currentLocation.streetName;
+
+    const mapRegion = {
+      latitude: (fromLoc.latitude + toLoc.latitude) / 2,
+      longitude: (fromLoc.longitude + toLoc.longitude) / 2,
+      latitudeDelta: Math.abs(fromLoc.latitude - toLoc.latitude) * 2,
+      longitudeDelta: Math.abs(fromLoc.longitude - toLoc.longitude) * 2,
+    };
+
+  function renderMap() {
+    const destinationMarker = () => (
+      <Marker coordinate={toLoc}>
+        <View
+          style={{
+            height: 30,
+            width: 30,
+            borderRadius: 20,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: COLORS.white,
+          }}
+        >
+          <View
+            style={{
+              height: 20,
+              width: 20,
+              borderRadius: 15,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: COLORS.primary,
+            }}
+          >
+            <Image
+              source={'../assets/img/marker.png'}
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: COLORS.white,
+              }}
+            />
+          </View>
+        </View>
+      </Marker>
     );
-  }
-}
-export default class Map extends Component {
-  componentDidMount() {
-    if (hasLocationPermission) {
-      Geolocation.getCurrentPosition(
-        (position) => {
-          console.log(position);
-        },
-        (error) => {
-          // See error code charts below.
-          console.log(error.code, error.message);
-        },
-        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-      );
-    }
-  }
-  render() {
+
+    const carIcon = () => (
+      <Marker
+        coordinate={fromLoc}
+        anchor={{x: 0.5, y: 0.5}}
+        flat={true}
+        rotation={angle}
+      >
+        <Image
+          source={'../assets/img/car.png'}
+          style={{
+            width: 40,
+            height: 40,
+          }}
+        />
+      </Marker>
+    );
+
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{flex: 1}}>
         <MapView
-          style={{ width: '100%', height: '100%' }}
-          showsUserLocation
-          loadingEnabled
+          ref={mapView}
           provider={PROVIDER_GOOGLE}
+          initialRegion={region}
+          style={{height: '100%', width: '100%'}}
           customMapStyle={mapStyle}
-        ></MapView>
+        >
+          <MapViewDirections
+            origin={fromLoc}
+            destination={toLoc}
+            apikey={GOOGLE_API_KEY}
+            strokeWidth={5}
+            strokeColor={COLORS.primary}
+            optimizeWaypoints={true}
+            onReady={result => {
+              setDuration(result.duration);
+
+              if (!isReady) {
+                // Fit route into maps
+                mapView.current.fitToCoordinates(result.coordinates, {
+                  edgePadding: {
+                    right: SIZES.width / 20,
+                    bottom: SIZES.height / 4,
+                    left: SIZES.width / 20,
+                    top: SIZES.height / 8,
+                  },
+                });
+                                   
+                setIsReady(true);
+              }
+            }}
+          />
+          {destinationMarker()}
+          {carIcon()}
+        </MapView>
         <View style={Style.mapDrawerOverlay} />
         <View style={Style.headerGps}>
           <Text style={Style.textGps}>Bem vindo, João!</Text>
@@ -283,4 +366,57 @@ export default class Map extends Component {
       </View>
     );
   }
-}
+
+  function renderDestinationHeader() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          top: 50,
+          left: 0,
+          right: 0,
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: SIZES.width * 0.9,
+            paddingVertical: SIZES.padding,
+            paddingHorizontal: SIZES.padding * 2,
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.white,
+          }}
+        >
+          <Image
+            source={'../assets/img/marker.png'}
+            style={{
+              width: 30,
+              height: 30,
+              marginRight: SIZES.padding,
+            }}
+          />
+
+          <View style={{flex: 1, marginTop: 30,}}>
+            <Text style={{...FONTS.body3}}>{street}</Text>
+          </View>
+
+          <Text style={{...FONTS.body3}}>{Math.ceil(duration)} mins</Text>
+        </View>
+      </View>
+    );
+  }
+
+
+  return (
+    <View style={{flex: 1}}>
+      {renderMap()}
+      {renderDestinationHeader()}
+    </View>
+  );
+};
+
+export default Gps;
